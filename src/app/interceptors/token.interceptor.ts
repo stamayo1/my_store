c453899dfa7f6d549131823c480423efa11ccb1f
stamayo <sebastian18t@outlook.com>
2022-03-19 22:06:26 -0500

    Update interceptos mange token

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -3,11 +3,20 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpContext, 
+  HttpContextToken
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import {TokenService} from '../services/token.service';
+
+const CHECK_TIME = new HttpContextToken<boolean>(() => false); 
+
+// Contexto que se carga en los request para validar si realizar o no, algo con el interceptor
+// export function checkTime(){
+//   return new HttpContext().set(CHECK_TIME, true)
+// }
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
@@ -17,6 +26,12 @@ export class TokenInterceptor implements HttpInterceptor {
   ) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    // CONTEXTO
+    // if (request.context.get(CHECK_TIME)){
+    //   request = this.addTokenHeader(request);
+    //   return next.handle(request);
+    // }
+
     request = this.addTokenHeader(request);
     return next.handle(request);
   }
--- src/app/models/Product.model.ts
+++ src/app/models/Product.model.ts
@@ -1,20 +1,20 @@
 export interface Category {
-    id: string,
-    name: string
+    id: string;
+    name: string;
 }
 
 export interface Product {
-    id: string,
-    title: string,
-    price: number, 
-    images: string[],
-    description: string ,
-    category: Category
+    id: string;
+    title: string;
+    price: number; 
+    images: string[];
+    description: string ;
+    category: Category;
 }
 
 
 export interface CreateProductDTO extends Omit<Product, "id" | "category"> {
-    categoryID: number    
+    categoryID: number;
 }
 
 // Para flexibilizar los campos  que se enviaran al back para actualizar, se usa el "?",
